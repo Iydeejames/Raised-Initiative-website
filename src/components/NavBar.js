eb5630b0757e57d4da660ebafd135e907694925b
Idaraobong James <iydeejaymes@gmail.com>
2023-12-13 21:44:57 +0100

    NavBar.js update

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,27 @@
-import {NavBar, Container } from "bootstrap"
+import {NavBar, Container, Button } from "bootstrap"
 export const NavBar = {} => {
     return(
 <Navbar  expand="lg">
 <Container>
   <Navbar.Brand href="#home">
-    <img src ={} alt ="logo" /> 
+    <img src ={''} alt ="logo" /> 
     </Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Toggle aria-controls="basic-navbar-nav"> </Navbar.Toggle>
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="me-auto">
       <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#link">Link</Nav.Link>
-      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2"> Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-      </NavDropdown>
+      <Nav.Link href="#link">What We Do</Nav.Link>
+      <Nav.Link href="#link">Events</Nav.Link>
+      <Nav.Link href="#link">Countries</Nav.Link>
     </Nav>
+    <span className="navbar-text">
+        <div className="social-icon">
+            <a href="#"><img src={} alt="" /> </a>
+            <a href="#"><img src={} alt="" /> </a>
+            <a href="#"><img src={} alt="" /> </a>
+        </div>
+        <Button className="contact" onClick={{} => console.log("connect")}><span>Contact Us</span>
+    </span>
   </Navbar.Collapse>
 </Container>
 </Navbar>
