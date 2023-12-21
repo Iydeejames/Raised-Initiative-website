e945030452cf8e98f61154567f591a9510bdde83
Idaraobong James <iydeejaymes@gmail.com>
2023-12-21 12:16:11 +0100

    Images added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { CustomNavbar } from './components/NavBar'; // Navbar is in the components folder
 
 function App() {
   return (
     <div className="App">
+      <CustomNavbar />
+      {/* Add other components/content here */}
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,49 +1,62 @@
-import {useEffect, useState} from "react";
-import {NavBar, Container, Button } from "bootstrap"
-export const NavBar = {} => {
-    const [activeLink, setActiveLink] =useState('home');
-    const [scolled, seScrolled] = useState[false];
+import React, { useEffect, useState } from "react";
+import { Navbar, Container, Nav } from "react-bootstrap"; // using react-bootstrap
 
-    useEffect(() => {
-        const onScroll =() => {
-            if (window.scrollY > 50) {
-                seScrolled(true);
-            } else {
-                seScrolled(false);
-            }
-        }
-        window.addEventListener("scroll", onScroll);
-        return() => window.removeEventListener("scroll", onScroll)
-    
-    }, [] )
+export const CustomNavbar = () => {
+  const [activeLink, setActiveLink] = useState("home");
+  const [scrolled, setScrolled] = useState(false);
 
-    const onUpdateActiveLink = (value) => {
-      setActiveLink(value);
-    }
-    return(
-<Navbar  expand="lg" className={ scrolled ? "scrolled": ""}>
-<Container>
-  <Navbar.Brand href="#home">
-    <img src ={''} alt ="logo" /> 
-    </Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav"> </Navbar.Toggle>
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="me-auto">
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  };
+
+  return (
+    <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
+      <Container>
+        <Navbar.Brand href="#home">
+          <img src={''} alt="Surpassing Mediocrity" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
       <Nav.Link href="#home" className={activeLink === "home" ? "active NavBar-link": ""} onClick={ () => onUpdateActiveLink ("home")} >Home</Nav.Link>
-      <Nav.Link href="#link" className={activeLink === "what we do" ? "active NavBar-link": ""} onClick={ () => onUpdateActiveLink ("what we do")}>What We Do</Nav.Link>
+      <Nav.Link href="#link" className={activeLink === "mission" ? "active NavBar-link": ""} onClick={ () => onUpdateActiveLink ("mission")}>Mission</Nav.Link>
       <Nav.Link href="#link" className={activeLink === "events" ? "active NavBar-link": ""} onClick={ () => onUpdateActiveLink ("events")}>Events</Nav.Link>
       <Nav.Link href="#link" className={activeLink === "countries" ? "active NavBar-link": ""} onClick={ () => onUpdateActiveLink ("countries")}>Countries</Nav.Link>
     </Nav>
-    <span className="navbar-text">
-        <div className="social-icon">
-            <a href="#"><img src={} alt="" /> </a>
-            <a href="#"><img src={} alt="" /> </a>
-            <a href="#"><img src={} alt="" /> </a>
-        </div>
-        <button className="contact"> onClick={() => console.log('connect')}</button><span>Contact Us</span>
-    </span>
-  </Navbar.Collapse>
-</Container>
-</Navbar>
-);
-}
+          <span className="navbar-text">
+            <div className="social-icon">
+              <a href="#">
+                <img src={''} alt="" />
+              </a>
+              <a href="#">
+                <img src={''} alt="" />
+              </a>
+              <a href="#">
+                <img src={''} alt="" />
+              </a>
+            </div>
+            <button className="contact" onClick={() => console.log('connect')}>
+              Contact Us
+            </button>
+          </span>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+
+
