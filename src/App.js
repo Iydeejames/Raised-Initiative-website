c193a49ef91f5ac9a87096bac0d5827464bbc26f
Idaraobong James <iydeejaymes@gmail.com>
2023-12-23 00:22:55 +0100

    New additions

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import'./App.css';
 import { CustomNavbar } from './components/NavBar'; // Navbar is in the components folder
 import HeroSection from './components/Hero';
+import BannerSection from './components/Banner';
 
 function App() {
   return (
     <div className="App">
       <CustomNavbar />
        <HeroSection />
+       <BannerSection />
       {/* Add other components/content here */}
     </div>
   );
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -8,6 +8,8 @@ const BannerSection = () => {
         <p>
           The mission of this organization is to be independent of any political ideology, religious doctrine, and economic interest but rather a value-adding organization driving the African development process through every positive means.
         </p>
+        </div>
+        <div className="Middle-content">
         <h3>Our Aims and Objectives:</h3>
         <ul>
           <li>To ensure the active participation of youths in the development of Africa.</li>
