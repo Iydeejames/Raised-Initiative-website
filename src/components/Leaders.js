48a21c693dad084cd54051f5c0a4b363dd5376e8
Idaraobong James <iydeejaymes@gmail.com>
2023-12-23 01:50:28 +0100

    File changed

--- src/components/Leaders.js
+++ src/components/Leaders.js
@@ -1,46 +1,33 @@
 import React, { useEffect } from "react";
 import gsap from "gsap";
 import RI1 from "../assets/img/RI1.jpg";
-import RI6 from "../assets/img/RI6.jpg";
+import RI6 from "../assets/img/RI6.JPG";
 import RI3 from "../assets/img/RI3.jpg";
 import RI2 from "../assets/img/RI2.jpg";
 
-const LeadersSection = () => {
-  useEffect(() => {
-    const tl = gsap.timeline({ defaults: { ease: "power2.inOut" } });
-
-    tl.from(".leader", { opacity: 0, y: 100, stagger: 0.2 });
-
-    // Trigger animations when the component mounts
-    tl.play();
-  }, []);
-
-  return (
-    <div className="leaders-section">
-      <div className="leader-frame">
-        <div className="leader">
-          <img src="RI1.jpg" alt="Leader 1" />
-          <p>Leader Name 1</p>
-        </div>
-        <div className="leader">
-          <img src="RI2.jpg" alt="Leader 2" />
-          <p>Leader Name 2</p>
-        </div>
-        <div className="leader">
-          <img src="RI3.jpg" alt="Leader 3" />
-          <p>Leader Name 3</p>
-        </div>
-        <div className="leader">
-          <img src="RI6.jpg" alt="Leader 4" />
-          <p>Leader Name 4</p>
-        </div>
-        <div className="leader">
-          <img src="leader5.jpg" alt="Leader 5" />
-          <p>Leader Name 5</p>
+const leadersData = [
+    { id: 1, name: "Leader Name 1", image: RI1 },
+    { id: 2, name: "Leader Name 2", image: RI2 },
+    { id: 3, name: "Leader Name 3", image: RI3 },
+    { id: 4, name: "Leader Name 4", image: RI6 },
+    // Add more leaders as needed
+  ];
+  
+  const LeadersSection = () => {
+    return (
+      <div className="leaders-section">
+        <div className="leader-frame">
+          {leadersData.map((leader) => (
+            <div key={leader.id} className="leader-card">
+              <img src={leader.image} alt={leader.name} className="leader-image" />
+              <div className="overlay">
+                <p className="leader-name">{leader.name}</p>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
-    </div>
-  );
-};
-
-export default LeadersSection;
+    );
+  };
+  
+  export default LeadersSection;
