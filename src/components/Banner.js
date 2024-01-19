09169e1807ae949d4c1090229080ed36193091bc
Idaraobong James <iydeejaymes@gmail.com>
2024-01-19 22:54:37 +0100

    Deletions and additions

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,14 +3,13 @@ import React from "react";
 const BannerSection = () => {
   return (
     <div className="banner-section">
-      <div className="left-content">
-        <h3>Our Mission:</h3>
+      <div className="content">
+        <h2>Our Mission:</h2>
         <p>
           The mission of this organization is to be independent of any political ideology, religious doctrine, and economic interest but rather a value-adding organization driving the African development process through every positive means.
         </p>
-        </div>
-        <div className="Middle-content">
-        <h3>Our Aims and Objectives:</h3>
+
+        <h2>Our Aims and Objectives:</h2>
         <ul>
           <li>To ensure the active participation of youths in the development of Africa.</li>
           <li>To develop a dynamic and disciplined African youth imbued with the spirit of nationalism, patriotism, and a sense of propriety and civic responsibility.</li>
@@ -18,9 +17,8 @@ const BannerSection = () => {
           <li>To raise conscious leaders to build a better Africa.</li>
           <li>To promulgate the concept of the sustainable development goals for an empowered Africa.</li>
         </ul>
-      </div>
-      <div className="right-content">
-        <h3>Our Values:</h3>
+
+        <h2>Our Values:</h2>
         <ul>
           <li>Think Africa above all.</li>
           <li>Respect for humanity, diversity, and teamwork.</li>
