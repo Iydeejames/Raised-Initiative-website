8a5f5cc2ab0301b8f7b54898bcc6d40bd658f936
Idaraobong James <iydeejaymes@gmail.com>
2024-02-01 18:09:44 +0100

    more edits

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,11 +11,11 @@ const BannerSection = () => {
 
         <h2>Our Aims and Objectives:</h2>
         <ul>
-          <li>To ensure the active participation of youths in the development of Africa.</li>
-          <li>To develop a dynamic and disciplined African youth imbued with the spirit of nationalism, patriotism, and a sense of propriety and civic responsibility.</li>
-          <li>To nourish, shape, and develop the innate potentials of the African youth.</li>
           <li>To raise conscious leaders to build a better Africa.</li>
+          <li>To ensure the active participation of youths in the development of Africa.</li>
+          <li>To nourish, shape, and develop the innate potentials of the African youth.</li>
           <li>To promulgate the concept of the sustainable development goals for an empowered Africa.</li>
+          <li>To develop a dynamic and disciplined African youth imbued with the spirit of nationalism, patriotism, and a sense of propriety and civic responsibility.</li>
         </ul>
 
         <h2>Our Values:</h2>
