f23b56a6119ec56bc8cf3af903c622e2923c4fb3
Idaraobong James <iydeejaymes@gmail.com>
2024-02-02 19:05:35 +0100

    Navbar edited

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import RI7 from "../assets/img/RI7.jpg";
 
-
 const CustomNavbar = () => {
   const [isMenuOpen, setMenuOpen] = useState(false);
   const [scrolled, setScrolled] = useState(false);
@@ -22,6 +21,11 @@ const CustomNavbar = () => {
     setMenuOpen(!isMenuOpen);
   };
 
+  const handleNavLinkClick = () => {
+    // Close the menu when a navigation link is clicked
+    setMenuOpen(false);
+  };
+
   return (
     <nav className={`navbar ${scrolled ? "scrolled" : ""}`}>
       <div className="container">
@@ -29,10 +33,10 @@ const CustomNavbar = () => {
           <img src={RI7} alt="Logo" className="logo-image" />
         </div>
         <div className={`nav-links ${isMenuOpen ? "active" : ""}`}>
-          <a href="#home">Home</a>
-          <a href="#leaders">Leaders</a>
-          <a href="#events">Events</a>
-          <a href="#contact">Contact Us</a>
+          <a href="#home" onClick={handleNavLinkClick}>Home</a>
+          <a href="#leaders" onClick={handleNavLinkClick}>Leaders</a>
+          <a href="#events" onClick={handleNavLinkClick}>Events</a>
+          <a href="#contact" onClick={handleNavLinkClick}>Contact Us</a>
         </div>
         <div className="menu-icon" onClick={toggleMenu}>
           <span></span>
